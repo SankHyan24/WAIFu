6cca194d49358ba636329a1e1832d402d2c4d106
Charles Sun <2209339564@qq.com>
2022-08-02 15:48:09 +0800

    the birth of my WAIFu

---
+++ README.md
@@ -0,0 +1,2 @@
+# WAIFu: a Weighted-Aligned Implicit Function
+Just a practice for implement a 3D reconstruction model like PIFu.
---
+++ main.py
@@ -0,0 +1,11 @@
+import torch
+import torch.nn as nn
+
+
+def main():
+    print("Hello World!")
+    print(torch.__version__)
+
+
+if __name__ == "__main__":
+    main()
