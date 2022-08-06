d6067b1df197b4c29aa79fb643a6432c8d4e5448
Charles Sun <2209339564@qq.com>
2022-08-06 15:41:20 +0800

    yes it can load and show mesh. update requirement.txt

--- README.md
+++ README.md
@@ -1,2 +1,31 @@
 # WAIFu: a Weighted-Aligned Implicit Function
+
+What is WAIFu?
+
+This is waifu:
+
+![820721](https://raw.githubusercontent.com/SankHyan24/image1/master/img/820721.jpg)
+
 Just a practice for implement a 3D reconstruction model like PIFu.
+
+## requirements.txt Backup:
+```txt
+# python 3.8 is recommended.
+
+matplotlib==3.5.2
+PyOpenGL==3.1.6 
+# use this if OpenGL.error.NullFunctionError: Attempt to call an undefined function glutInit. 
+# https://www.lfd.uci.edu/~gohlke/pythonlibs/#pyopengl  PyOpenGL‑3.1.6‑cp38‑cp38‑win_amd64.whl
+torch==1.12.0 
+# use this if cuda is not available. 
+# https://download.pytorch.org/whl/cu116/torch-1.12.0%2Bcu116-cp38-cp38-win_amd64.whl
+torchvision==0.13.0
+opencv-python # 4.6.0.66, for example
+```
+
+## How to Update "requirements.txt"?
+use this command
+```shell
+pipreqs ./ --encoding=utf8 --force
+```
+
--- render.py
+++ render.py
@@ -1,17 +1,31 @@
 from OpenGL.GL import *
 from OpenGL.GLU import *
 from OpenGL.GLUT import *
- 
-def Draw():
-    glClear(GL_COLOR_BUFFER_BIT)
-    glRotatef(0.5, 0, 1, 0)
-    glutWireTeapot(0.5)
-    glFlush()
- 
-glutInit()
-glutInitDisplayMode(GLUT_SINGLE | GLUT_RGBA)
-glutInitWindowSize(400, 400)
-glutCreateWindow("test")
-glutDisplayFunc(Draw)
-glutIdleFunc(Draw)
-glutMainLoop() 
+import trimesh
+import os
+
+def load_trimesh(root_dir):
+    folders = os.listdir(root_dir)
+    meshs = trimesh.load(os.path.join(root_dir, 'model.obj'))
+
+    return meshs
+
+class train:
+    def __init__(self) -> None:
+        self.mesh_dic=load_trimesh("./Datas")
+    
+a=train()
+a.mesh_dic.show()
+
+# def Draw():
+#     glClear(GL_COLOR_BUFFER_BIT)
+#     glRotatef(0.5, 0, 1, 0)
+#     glutWireTeapot(0.5)
+#     glFlush()
+# glutInit()
+# glutInitDisplayMode(GLUT_SINGLE | GLUT_RGBA)
+# glutInitWindowSize(400, 400)
+# glutCreateWindow("test")
+# glutDisplayFunc(Draw)
+# glutIdleFunc(Draw)
+# glutMainLoop() 
